2e207aea179aa55e6c74f0e2682ee750c0329c11
josep <boobooboo2@naver.com>
2022-11-16 10:19:30 +0900

    button 모듈 property naming 수정

--- modi_plus/module/input_module/button.py
+++ modi_plus/module/input_module/button.py
@@ -9,12 +9,12 @@ class Button(InputModule):
     STATE_TRUE = 100
     STATE_FALSE = 0
 
-    PROP_BUTTON_STATE = 2
+    PROPERTY_BUTTON_STATE = 2
 
-    PROP_OFFSET_CLICKED = 0
-    PROP_OFFSET_DOUBLE_CLICKED = 2
-    PROP_OFFSET_PRESSED = 4
-    PROP_OFFSET_TOGGLED = 6
+    PROPERTY_OFFSET_CLICKED = 0
+    PROPERTY_OFFSET_DOUBLE_CLICKED = 2
+    PROPERTY_OFFSET_PRESSED = 4
+    PROPERTY_OFFSET_TOGGLED = 6
 
     @property
     def clicked(self) -> bool:
@@ -24,8 +24,8 @@ class Button(InputModule):
         :rtype: bool
         """
 
-        offset = Button.PROP_OFFSET_CLICKED
-        raw = self._get_property(Button.PROP_BUTTON_STATE)
+        offset = Button.PROPERTY_OFFSET_CLICKED
+        raw = self._get_property(Button.PROPERTY_BUTTON_STATE)
         data = struct.unpack("H", raw[offset:offset+2])[0]
         return data == Button.STATE_TRUE
 
@@ -37,8 +37,8 @@ class Button(InputModule):
         :rtype: bool
         """
 
-        offset = Button.PROP_OFFSET_DOUBLE_CLICKED
-        raw = self._get_property(Button.PROP_BUTTON_STATE)
+        offset = Button.PROPERTY_OFFSET_DOUBLE_CLICKED
+        raw = self._get_property(Button.PROPERTY_BUTTON_STATE)
         data = struct.unpack("H", raw[offset:offset+2])[0]
         return data == Button.STATE_TRUE
 
@@ -50,8 +50,8 @@ class Button(InputModule):
         :rtype: bool
         """
 
-        offset = Button.PROP_OFFSET_PRESSED
-        raw = self._get_property(Button.PROP_BUTTON_STATE)
+        offset = Button.PROPERTY_OFFSET_PRESSED
+        raw = self._get_property(Button.PROPERTY_BUTTON_STATE)
         data = struct.unpack("H", raw[offset:offset+2])[0]
         return data == Button.STATE_TRUE
 
@@ -63,7 +63,7 @@ class Button(InputModule):
         :rtype: bool
         """
 
-        offset = Button.PROP_OFFSET_TOGGLED
-        raw = self._get_property(Button.PROP_BUTTON_STATE)
+        offset = Button.PROPERTY_OFFSET_TOGGLED
+        raw = self._get_property(Button.PROPERTY_BUTTON_STATE)
         data = struct.unpack("H", raw[offset:offset+2])[0]
         return data == Button.STATE_TRUE
