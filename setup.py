0495d3cb58f5267b2ebaa2f4d8795c507dfdaf94
Jihun Kim <lego8421@gmail.com>
2022-11-16 10:56:43 +0900

    python req version 수정

--- setup.py
+++ setup.py
@@ -53,10 +53,9 @@ setup(
         "Intended Audience :: Education",
         "Intended Audience :: Information Technology",
         "Intended Audience :: Science/Research",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    python_requires='>=3.10',
 )
